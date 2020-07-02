2ca285fa614bd504b43759adcabaf4580c342437
odilo <odilorugamba@gmail.com>
2020-07-02 12:28:34 +0200

    changed Response variable in validate.js

--- src/Utils/validate.js
+++ src/Utils/validate.js
@@ -1,11 +1,11 @@
 import Joi from '@hapi/joi';
-import utils from './response';
+import Response from './response';
 
 const validate = (object, schema, req, res, next) => {
   const { error, value } = Joi.validate(object, schema, { abortEarly: false });
 
   if (error) {
-    return utils.Error(400, res, error.details.map((detail) => {
+    return Response.Error(400, res, error.details.map((detail) => {
       const message = detail.message.replace(/"/gi, '');
       return message;
     }));
